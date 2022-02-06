fa1c304d8273a40ef29daad3f9eecb2257aa4f03
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-02-06 15:15:02 +0900

    Update

--- denops/@ddu-sources/file.ts
+++ denops/@ddu-sources/file.ts
@@ -1,11 +1,11 @@
 import {
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v0.3.0/types.ts#^";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.3.0/deps.ts#^";
-import { join, resolve } from "https://deno.land/std@0.122.0/path/mod.ts";
+} from "https://deno.land/x/ddu_vim@v0.7.1/types.ts#^";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.7.1/deps.ts#^";
+import { join, resolve } from "https://deno.land/std@0.125.0/path/mod.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
-import { relative } from "https://deno.land/std@0.122.0/path/mod.ts#^";
+import { relative } from "https://deno.land/std@0.125.0/path/mod.ts#^";
 
 type Params = {
   path: string;
