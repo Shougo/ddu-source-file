5a4efa76549f711e8f327e9ae67b3d84d4c029e3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-10-07 09:45:42 +0900

    Fix deps

--- denops/@ddu-sources/file.ts
+++ denops/@ddu-sources/file.ts
@@ -2,11 +2,11 @@ import {
   BaseSource,
   Item,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v2.5.2/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.5.2/deps.ts";
-import { join, resolve } from "https://deno.land/std@0.155.0/path/mod.ts";
+} from "https://deno.land/x/ddu_vim@v1.11.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v1.11.0/deps.ts";
+import { join, resolve } from "https://deno.land/std@0.159.0/path/mod.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.1/file.ts";
-import { relative } from "https://deno.land/std@0.155.0/path/mod.ts";
+import { relative } from "https://deno.land/std@0.159.0/path/mod.ts";
 
 type Params = {
   "new": boolean;
